5f8e2e29bbd95f35b0f406e73e4b7a96ad43d07d
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-04-30 14:44:27 -0700

    Add placeholder functions for CLI.

---
+++ src/simplecanvas/cli.py
@@ -0,0 +1,11 @@
+def newcourse(name):
+    """Create a new course from templates."""
+    pass
+
+def addmod(name):
+    """Add a module with template files."""
+    pass
+
+def upmod(name):
+    """Upload a module to Canvas through API calls."""
+    pass
