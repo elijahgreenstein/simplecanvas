4d9d1e7daa6af20a2598779221b609ea21e46ede
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-04-30 15:05:10 -0700

    Style

--- src/simplecanvas/__main__.py
+++ src/simplecanvas/__main__.py
@@ -2,24 +2,21 @@ import argparse
 
 from .cli import newcourse, addmod, upmod
 
+
 def main():
     parser = argparse.ArgumentParser(
         prog="Simple Canvas",
-        description="Create a course, add a module, or upload a module."
+        description="Create a course, add a module, or upload a module.",
     )
-    parser.add_argument("-v", "--verbose", help="increase verbosity level", action="count")
-    subparsers = parser.add_subparsers(
-        #metavar="command",
-        #help="simplecanvas action",
-        required=True
+    parser.add_argument(
+        "-v", "--verbose", help="increase verbosity level", action="count"
     )
+    subparsers = parser.add_subparsers(required=True)
     parser_newcourse = subparsers.add_parser(
         "newcourse", help="create new course"
     )
     parser_newcourse.set_defaults(func=newcourse)
-    parser_addmod = subparsers.add_parser(
-        "addmod", help="add module to course"
-    )
+    parser_addmod = subparsers.add_parser("addmod", help="add module to course")
     parser_addmod.set_defaults(func=addmod)
     parser_upmod = subparsers.add_parser(
         "upmod", help="upload module to Canvas"
@@ -29,6 +26,6 @@ def main():
     args = parser.parse_args()
     args.func(args.name)
 
+
 if __name__ == "__main__":
     main()
-
