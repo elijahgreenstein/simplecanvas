43769c707797b20f532e0eea50b460c5861ae5f1
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-05-01 12:09:35 -0700

    Add Page, Discussion, Quiz, QuizQuestion objects

--- src/simplecanvas/objects.py
+++ src/simplecanvas/objects.py
@@ -60,6 +60,7 @@ class Item:
         self.body_name = None
         self.id = None
         self.id_name = None
+        self.param = None
 
     def set_id(self, uid):
         self.id = uid
@@ -70,3 +71,74 @@ class Item:
         settings[self.body_name] = self.body
         settings = {self.param: settings} if self.param else settings
         return settings
+
+
+class Page(Item):
+
+    def __init__(self, title, body):
+        super().__init__(title, body)
+        self.type = "Page"
+        self.path = "pages"
+        self.body_name = "body"
+        self.param = "wiki_page"
+        self.id_name = "url"
+        self.content_name = "page_url"
+
+
+class Discussion(Item):
+
+    def __init__(self, title, body, settings):
+        super().__init__(title, body)
+        self.type = "Discussion"
+        self.path ="discussion_topics"
+        self.settings = settings
+        self.body_name = "message"
+self.param = None
+        self.id_name = "id"
+        self.content_name = "content_id"
+
+
+class Quiz(Item):
+
+    def __init__(self, title, body, settings, questions):
+        super().__init__(title, body)
+        self.type = "Quiz"
+        self.path = "quizzes"
+        self.settings = settings
+        self.body_name = "description"
+        self.questions = questions
+        self.param = "quiz"
+        self.id_name = "id"
+        self.content_name = "content_id"
+
+
+class QuizQuestion:
+
+    def __init__(self, question, correct=[], incorrect=[]):
+        self.question = question
+        self.correct = correct
+        self.incorrect = incorrect
+        if not correct and not incorrect:
+            self.type = "essay_question"
+        elif len(correct) > 1:
+            self.type = "multiple_answers_question"
+        else:
+            self.type = "multiple_choice_question"
+
+    def get_json(self):
+        res = {
+            "question_name": "Question",
+            "question_type": self.type,
+            "points_possible": 1,
+            "question_text": self.question,
+        }
+        answers = []
+        for answer in self.correct:
+            answers.append({"answer_text": answer, "answer_weight": 100.0})
+        for answer in self.incorrect:
+            answers.append({"answer_text": answer, "answer_weight": 0.0})
+        if answers:
+            res["answers"] = answers
+        else:
+            pass
+        return res
