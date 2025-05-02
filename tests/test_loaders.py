7ee787d0cfb501e8ae71620c35affdba4f47e4ec
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-05-02 14:32:00 -0700

    Prepare test for module loader

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -14,6 +14,11 @@ def user_example():
     return objects.User("12345ABCDE")
 
 
+@pytest.fixture
+def module_example():
+    return objects.Module("A test module", 3)
+
+
 def test_load_user(user_example):
     res = loaders.load_user(TEST101 / FS.token)
     assert res.token == user_example.token
@@ -22,7 +27,7 @@ def test_load_user(user_example):
 
 def test_load_course():
     res = loaders.load_course(TEST101 / FS.cset, TEST101 / FS.qdesc)
-    assert res.id == "987"
+    assert res.uid == "987"
     assert res.url.geturl() == "example/api"
     assert res.path == Path("example/api/courses/987")
     assert res.disc == {
@@ -34,3 +39,15 @@ def test_load_course():
         "quiz_type": "assignment",
         "shuffle_answers": True,
     }
+
+
+def test_load_module(module_example):
+    res = loaders.load_module(TEST101 / FS.mod / "W01")
+    assert module_example.title == res.title
+    assert module_example.position == res.position
+    assert res.path == "modules"
+    assert res.uid == None
+    assert res.id_name == "id"
+    new_id == "123456"
+    res.set_id(new_id)
+    assert res.uid == new_id
