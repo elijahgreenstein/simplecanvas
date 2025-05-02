cfe5b7ab1b75b70eeff44ec3f0dd327bb1aec586
Elijah Greenstein <197816462+elijahgreenstein@users.noreply.github.com>
2025-05-02 14:08:11 -0700

    Set up test for course load

--- tests/test_loaders.py
+++ tests/test_loaders.py
@@ -18,3 +18,19 @@ def test_load_user(user_example):
     res = loaders.load_user(TEST101 / FS.token)
     assert res.token == user_example.token
     assert res.auth == user_example.auth
+
+
+def test_load_course():
+    res = loaders.load_course(TEST101 / FS.cset, TEST101 / FS.qdesc)
+    assert res.id == "987"
+    assert res.url = "example/api"
+    assert res.path = Path("example/api/courses/987")
+    assert res.disc = {
+        "discussion_type": "threaded",
+        "published": False,
+    }
+    assert res.quiz = {
+        "hide_results": "always",
+        "quiz_type": "assignment",
+        "shuffle_answers": True,
+    }
